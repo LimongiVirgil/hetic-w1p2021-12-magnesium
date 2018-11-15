ef1e360bbcf802577e0dad33c5261df2ae36cf24
Virgil Limongi <virgillimongi@MacBook-Pro-de-Virgil.local>
2018-11-15 17:56:00 +0100

    site version final v1

---
+++ css/déroulement.js
@@ -0,0 +1,10 @@
+function voirsuite() {
+
+  document.getElementById('voirsuite').style.display='none';
+ document.getElementById('suite').style.display='block';
+}
+function replier() {
+
+ document.getElementById('voirsuite').style.display='block';
+  document.getElementById('suite').style.display='none';
+}
